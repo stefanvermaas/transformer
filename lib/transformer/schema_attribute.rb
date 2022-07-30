f4b0f0519047f17a3df4e134c5833c57ae46c049
Stefan Vermaas <stefanvermaas@pm.me>
2022-07-30 13:15:36 +0200

    Allow reading from a context and cast the return values

--- lib/transformer/schema_attribute.rb
+++ lib/transformer/schema_attribute.rb
@@ -2,11 +2,36 @@
 
 module Transformer
   class SchemaAttribute
-    attr_reader :cast_type, :name
+    attr_reader :cast_type, :name, :value_before_read
 
-    def initialize(name, cast_type: nil)
+    def initialize(name, cast_type: nil, value_before_read: nil)
       @name = name.to_sym
       @cast_type = cast_type.to_sym if cast_type
+      @value_before_read = value_before_read || @name
+    end
+
+    # Extracts and casts the value from the data source. The given block is passed
+    # to the extraction methods as the context.
+    #
+    # @example supply context to the `value` method.
+    #
+    #   class DataSource
+    #     def data
+    #       { email: "jane@example.com" }
+    #     end
+    #   end
+    #
+    #   $ schema_attribute = SchemaAttribute.new(:email)
+    #   = > <Transformer::SchemaAttribute ... />
+    #
+    #   $ schema_attribute.value { DataSource.new }
+    #   => "jane@example.com"
+    #
+    # @return [Any]
+    def value
+      return @value if defined?(@value)
+
+      @value = extract_and_cast_value(yield) if block_given?
     end
 
     # The `#==` allows using the `Array#|` concatination assignment as under the hood,
@@ -18,5 +43,41 @@ module Transformer
       name == other.name
     end
     alias eql? ==
+
+    private
+
+    def cast_value(value) # rubocop:disable Metrics/CyclomaticComplexity
+      case @cast_type
+      when :array then value.to_a
+      when :big_decimal, :bigdecimal then BigDecimal(value)
+      when :float then value.to_f
+      when :hash then value.to_h { |item| [item[0], item[1]] }
+      when :integer then value.to_i
+      when :string then value.to_s
+      else
+        value
+      end
+    end
+
+    def extract_and_cast_value(context)
+      value_before_type_cast = extract_value_from_context(context)
+      cast_value(value_before_type_cast)
+    end
+
+    def extract_value_from_context(context) # rubocop:disable Metrics/MethodLength
+      case @value_before_read
+      when Proc
+        context.instance_exec(&@value_before_read)
+      when String
+        context.data.dig(*@value_before_read.split(".")) ||
+          context.data.dig(*@value_before_read.split(".").map(&:to_sym))
+      when Symbol
+        if context.respond_to?(@value_before_read, true)
+          context.send(@value_before_read)
+        else
+          context.data[@value_before_read]
+        end
+      end
+    end
   end
 end
--- test/transformer/schema_attribute_test.rb
+++ test/transformer/schema_attribute_test.rb
@@ -26,5 +26,129 @@ module Transformer
       schema_attribute = SchemaAttribute.new(:email)
       assert_nil schema_attribute.cast_type
     end
+
+    def test_defaults_to_name_as_value_before_read
+      schema_attribute = SchemaAttribute.new(:email)
+      assert_equal schema_attribute.name, schema_attribute.value_before_read
+    end
+
+    class SchemaAttributeValueTest < MiniTest::Test
+      def setup
+        @context = Class.new do
+          def data
+            {
+              id: 123,
+              email: "jane@example.com",
+              first_name: "Jane",
+              last_name: "Joules",
+              metadata: {
+                username: "jane89",
+                user_profile_id: "456",
+                hour_rate: "100.00"
+              }
+            }
+          end
+
+          def full_name
+            "#{data[:first_name]} #{data[:last_name]}"
+          end
+        end
+      end
+
+      def test_value_without_context_returns_nil
+        schema_attribute = SchemaAttribute.new(:email)
+        assert_nil schema_attribute.value
+      end
+
+      def test_value_is_extractable_as_string_from_symbol_hash_key
+        schema_attribute = SchemaAttribute.new(:email, value_before_read: "email")
+        assert_equal("jane@example.com", schema_attribute.value { @context.new })
+      end
+
+      def test_value_is_extractable_as_string_from_string_hash_key
+        context = Class.new do
+          def data
+            { "email" => "jane@example.com" }
+          end
+        end
+
+        schema_attribute = SchemaAttribute.new(:email, value_before_read: "email")
+        assert_equal("jane@example.com", schema_attribute.value { context.new })
+      end
+
+      def test_value_is_extractable_as_string_from_nested_hash
+        schema_attribute = SchemaAttribute.new(:username, value_before_read: "metadata.username")
+        assert_equal("jane89", schema_attribute.value { @context.new })
+      end
+
+      def test_value_is_extractable_as_symbol_method_reference
+        schema_attribute = SchemaAttribute.new(:full_name, value_before_read: :full_name)
+        assert_equal("Jane Joules", schema_attribute.value { @context.new })
+      end
+
+      def test_value_is_extractable_as_symbol_for_root_hash
+        schema_attribute = SchemaAttribute.new(:first_name, value_before_read: :first_name)
+        assert_equal("Jane", schema_attribute.value { @context.new })
+      end
+
+      def test_value_is_extractable_as_proc_with_static_value
+        schema_attribute = SchemaAttribute.new(:static, value_before_read: -> { "static" })
+        assert_equal("static", schema_attribute.value { @context.new })
+      end
+
+      def test_value_is_extractable_as_proc_with_dynamic_value
+        schema_attribute = SchemaAttribute.new(:first_name,
+                                               value_before_read: -> { data[:first_name] })
+
+        assert_equal("Jane", schema_attribute.value { @context.new })
+      end
+
+      def test_value_is_not_casted_without_cast_type
+        schema_attribute = SchemaAttribute.new(:first_name)
+        assert_equal(String, schema_attribute.value { @context.new }.class)
+      end
+
+      def test_value_is_castable_to_string
+        schema_attribute = SchemaAttribute.new(:id, cast_type: :string)
+        assert_equal(String, schema_attribute.value { @context.new }.class)
+      end
+
+      def test_value_is_castable_to_integer
+        schema_attribute = SchemaAttribute.new(:user_profile_id,
+                                               cast_type: :integer,
+                                               value_before_read: "metadata.user_profile_id")
+
+        assert_equal(Integer, schema_attribute.value { @context.new }.class)
+      end
+
+      def test_value_is_castable_to_float
+        schema_attribute = SchemaAttribute.new(:hour_rate,
+                                               cast_type: :float,
+                                               value_before_read: "metadata.hour_rate")
+
+        assert_equal(Float, schema_attribute.value { @context.new }.class)
+      end
+
+      def test_value_is_castable_to_big_decimal
+        schema_attribute = SchemaAttribute.new(:hour_rate,
+                                               cast_type: :big_decimal,
+                                               value_before_read: "metadata.hour_rate")
+
+        assert_equal(BigDecimal, schema_attribute.value { @context.new }.class)
+      end
+
+      def test_value_is_castable_to_array
+        schema_attribute = SchemaAttribute.new(:metadata, cast_type: :array)
+        assert_equal(Array, schema_attribute.value { @context.new }.class)
+      end
+
+      def test_value_is_castable_to_hash
+        schema_attribute = SchemaAttribute.new(:first_name,
+                                               cast_type: :hash,
+                                               value_before_read: -> { [:first_name, data[:first_name]] })
+
+        assert_equal(Hash, schema_attribute.value { @context.new }.class)
+      end
+    end
   end
 end

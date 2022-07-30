106b67f0cfba6f01fe9ce29c6ea07c6ab39f49fd
Stefan Vermaas <stefanvermaas@pm.me>
2022-07-30 17:49:52 +0200

    Add suport for selective attribute reading

--- lib/transformer/attribute_set.rb
+++ lib/transformer/attribute_set.rb
@@ -9,14 +9,39 @@ module Transformer
       @attributes = context.attributes
     end
 
-    def to_h
-      @attributes.each_with_object({}) do |attribute, hash|
-        hash[attribute.name] = attribute.value { @context }
+    def attribute_names
+      _attributes.keys
+    end
+
+    def to_h(**options)
+      attribute_names_for_serialization = attribute_names
+
+      if options[:only]
+        attribute_names_for_serialization &= Array(options[:only]).map(&:to_sym)
+      elsif options[:except]
+        attribute_names_for_serialization -= Array(options[:except]).map(&:to_sym)
       end
+
+      read_attributes(attribute_names_for_serialization)
     end
 
     def to_json(**options)
       JSON.generate(to_h, **options)
+    end
+
+    private
+
+    def _attributes
+      @_attributes ||=
+        @attributes.each_with_object({}) do |attribute, hash|
+          hash[attribute.name] = attribute
+        end
+    end
+
+    def read_attributes(attribute_names_for_serialization)
+      _attributes.slice(*attribute_names_for_serialization).transform_values do |attribute|
+        attribute.value { @context }
+      end
     end
   end
 end
--- test/transformer/attribute_set_test.rb
+++ test/transformer/attribute_set_test.rb
@@ -9,24 +9,41 @@ module Transformer
         schema do
           attribute :first_name
           attribute :last_name
+          attribute :email
         end
       end
+
+      @user_hash = { first_name: "Jane", last_name: "Doe", email: "jane@example.com" }
     end
 
     def test_read_and_cast_attributes
-      user_hash = { first_name: "Jane", last_name: "Doe" }
-      user_mapping = @model.new(user_hash).to_h
+      user_mapping = @model.new(@user_hash).to_h
 
       assert_equal "Jane", user_mapping[:first_name]
       assert_equal "Doe", user_mapping[:last_name]
     end
 
     def test_turns_attribute_set_to_json
-      user_hash = { first_name: "Jane", last_name: "Doe" }
-      user_mapping = @model.new(user_hash).to_json
+      user_mapping = @model.new(@user_hash).to_json
 
       assert_equal String, user_mapping.class
-      assert_equal user_hash.to_json, user_mapping
+      assert_equal @user_hash.to_json, user_mapping
+    end
+
+    def test_casting_selected_attributes_only
+      user_mapping = @model.new(@user_hash).to_h(only: :first_name)
+
+      assert_nil user_mapping[:last_name]
+      assert_nil user_mapping[:email]
+      assert_equal "Jane", user_mapping[:first_name]
+    end
+
+    def test_excepted_attributes
+      user_mapping = @model.new(@user_hash).to_h(except: %i[first_name email])
+
+      assert_nil user_mapping[:first_name]
+      assert_nil user_mapping[:email]
+      assert_equal "Doe", user_mapping[:last_name]
     end
   end
 end

1d40076d46b82ab6fb89dec97373ffcd9ecb201c
Stefan Vermaas <stefanvermaas@pm.me>
2022-07-31 10:02:25 -0400

    Make the Transformer::AttributeSet more resilent.
    Improves the Transformer::AttributeSet by:
    - Adding an attribute cache which will make subsequantial reads faster.
    - Relying on explicit data passed into the class.
    - Adding the :only and :except filter options to limit the output.
    - Allow passing options for JSON serialization.

--- lib/transformer/attribute_set.rb
+++ lib/transformer/attribute_set.rb
@@ -3,16 +3,36 @@
 require "json"
 
 module Transformer
+  # The `Transformer::AttributeSet` builds the outputs for a `Transformer::Model`
+  # based on information from the `Transformer::SchemaDefinition`.
   class AttributeSet
-    def initialize(context)
+    def initialize(context, schema_definition)
       @context = context
-      @attributes = context.attributes
+      @schema_definition = schema_definition
     end
 
-    def attribute_names
-      _attributes.keys
-    end
-
+    # Transforms the `Transformer::Model` to a hash based on the defined schema.
+    #
+    # @example transformation for only a few attributes
+    #
+    #   class UserMapping < Transformer::Model
+    #     schema do
+    #       attribute :first_name
+    #       attribute :last_name
+    #     end
+    #   end
+    #
+    #   $ user_mapping = UserMapping.new({ first_name: "Jane", last_name: "Doe" })
+    #   $ user_mapping.to_h(only: :first_name)
+    #   => { first_name: "Jane" }
+    #
+    # @example transformation with all attributes except some
+    #
+    #   $ user_mapping.to_h(except: :first_name)
+    #   => { last_name: "Jane" }
+    #
+    # @param options [Hash] A list of options for serialization (e.g. :only or :except).
+    # @return [Hash]
     def to_h(**options)
       attribute_names_for_serialization = attribute_names
 
@@ -22,25 +42,39 @@ module Transformer
         attribute_names_for_serialization -= Array(options[:except]).map(&:to_sym)
       end
 
-      read_attributes(attribute_names_for_serialization)
+      attributes_for_serialization = attribute_cache.slice(*attribute_names_for_serialization)
+      read_and_cache_attributes!(attributes_for_serialization)
     end
 
+    # Transforms the `Transformer::Model` to a JSON string based on the defined schema.
+    #
+    # NOTE: The serialization options can also include options for the JSON serializer
+    # and for the `Transformer::AttributeSet#to_h` method.
+    #
+    # @param options [Hash] A list of serialization options for serialization.
+    # @return [String]
     def to_json(**options)
-      JSON.generate(to_h, **options)
+      filter_options = options.slice(:only, :except)
+      json_options = options.except(:only, :except)
+
+      JSON.generate(to_h(**filter_options), **json_options)
     end
 
     private
 
-    def _attributes
-      @_attributes ||=
-        @attributes.each_with_object({}) do |attribute, hash|
-          hash[attribute.name] = attribute
-        end
+    def attribute_cache
+      @attribute_cache ||= @schema_definition.each_with_object({}) do |attribute, hash|
+        hash[attribute.name] = attribute
+      end
     end
 
-    def read_attributes(attribute_names_for_serialization)
-      _attributes.slice(*attribute_names_for_serialization).transform_values do |attribute|
-        attribute.value { @context }
+    def attribute_names
+      @attribute_names ||= attribute_cache.keys
+    end
+
+    def read_and_cache_attributes!(attributes_for_serialization)
+      attributes_for_serialization.transform_values! do |attribute|
+        attribute.is_a?(SchemaAttribute) ? attribute.value { @context } : attribute
       end
     end
   end
--- test/transformer/attribute_set_test.rb
+++ test/transformer/attribute_set_test.rb
@@ -5,45 +5,59 @@ require "test_helper"
 module Transformer
   class AttributeSetTest < MiniTest::Test
     def setup
-      @model = Class.new Transformer::Model do
-        schema do
-          attribute :first_name
-          attribute :last_name
-          attribute :email
-        end
-      end
+      @data = { first_name: "Jane", last_name: "Doe", email: "jane@example.com" }
+      context = Struct.new(:data).new(@data)
 
-      @user_hash = { first_name: "Jane", last_name: "Doe", email: "jane@example.com" }
+      schema_definition = SchemaDefinition.new
+      schema_definition.attributes :first_name, :last_name, :email
+
+      @attribute_set = AttributeSet.new(context, schema_definition)
     end
 
     def test_read_and_cast_attributes
-      user_mapping = @model.new(@user_hash).to_h
+      output = @attribute_set.to_h
 
-      assert_equal "Jane", user_mapping[:first_name]
-      assert_equal "Doe", user_mapping[:last_name]
+      assert_equal "Jane", output[:first_name]
+      assert_equal "Doe", output[:last_name]
     end
 
     def test_turns_attribute_set_to_json
-      user_mapping = @model.new(@user_hash).to_json
+      output = @attribute_set.to_json
 
-      assert_equal String, user_mapping.class
-      assert_equal @user_hash.to_json, user_mapping
+      assert_equal String, output.class
+      assert_equal @data.to_json, output
     end
 
     def test_casting_selected_attributes_only
-      user_mapping = @model.new(@user_hash).to_h(only: :first_name)
+      output = @attribute_set.to_h(only: :first_name)
 
-      assert_nil user_mapping[:last_name]
-      assert_nil user_mapping[:email]
-      assert_equal "Jane", user_mapping[:first_name]
+      assert_nil output[:last_name]
+      assert_nil output[:email]
+      assert_equal "Jane", output[:first_name]
     end
 
     def test_excepted_attributes
-      user_mapping = @model.new(@user_hash).to_h(except: %i[first_name email])
+      output = @attribute_set.to_h(except: %i[first_name email])
 
-      assert_nil user_mapping[:first_name]
-      assert_nil user_mapping[:email]
-      assert_equal "Doe", user_mapping[:last_name]
+      assert_nil output[:first_name]
+      assert_nil output[:email]
+      assert_equal "Doe", output[:last_name]
+    end
+
+    def test_casting_selected_attributes_only_for_to_json
+      json_output = @attribute_set.to_json(only: :first_name)
+
+      assert_match(/jane/i, json_output)
+      refute_match(/doe/i, json_output)
+      refute_match(/jane@example.com/i, json_output)
+    end
+
+    def test_excepted_attributes_for_to_json
+      json_output = @attribute_set.to_json(except: :first_name)
+
+      refute_match(/jane$/i, json_output)
+      assert_match(/doe/i, json_output)
+      assert_match(/jane@example.com/i, json_output)
     end
   end
 end

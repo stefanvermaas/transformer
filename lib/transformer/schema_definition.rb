66ffee940ffa6339f199cdcf2400e923d4e400fb
Stefan Vermaas <stefanvermaas@pm.me>
2022-08-14 07:09:54 +0200

    Make the Transformer::SchemaDefinition iterable

--- lib/transformer/schema_definition.rb
+++ lib/transformer/schema_definition.rb
@@ -1,19 +1,53 @@
 # frozen_string_literal: true
 
 module Transformer
+  # The `Transformer::SchemaDefinition` collects all information about the schema
+  # of the `Transformer::Model` and its attributes.
   class SchemaDefinition
+    include Enumerable
+
     def initialize
       @attributes = []
     end
 
-    def attribute(name)
-      @attributes |= [SchemaAttribute.new(name)]
+    # Adds a single attribute to the `Transformer::SchemaDefinition` for the data
+    # transformer class.
+    #
+    # @name [String|Symbol] The name for the key in the output hash.
+    # @options [Hash] A list of options for the attribute (e.g. type and value).
+    # @return [Array]
+    def attribute(name, **options)
+      @attributes |= [
+        SchemaAttribute.new(
+          name,
+          cast_type: options[:type],
+          value_before_read: options[:value]
+        )
+      ]
     end
 
-    def attributes(*names, **kwargs)
-      return @attributes if names.empty?
-
-      names.each { |name| attribute(name, **kwargs) }
+    # Adds a list of attributes to the `Transformer::SchemaDefinition` and uses
+    # the same set of options for each attribute. Ideal for mass-assignment.
+    #
+    # @param names [Array] A list of attribute names.
+    # @param options [Hash] A list of options for the attribute (e.g. type and value).
+    # @return [Array]
+    def attributes(*names, **options)
+      names.each { |name| attribute(name, **options) }
     end
+
+    def each(&block)
+      @attributes.each(&block)
+    end
+
+    def include?(attribute)
+      @attributes.include?(attribute)
+    end
+
+    def size
+      @attributes.size
+    end
+    alias length size
+    alias count size
   end
 end
--- test/transformer/schema_definition_test.rb
+++ test/transformer/schema_definition_test.rb
@@ -11,10 +11,10 @@ module Transformer
 
     def test_adding_attributes
       schema_definition = SchemaDefinition.new
-      refute_includes schema_definition.attributes, SchemaAttribute.new(:email)
+      refute_includes schema_definition, SchemaAttribute.new(:email)
 
       schema_definition.attribute(:email)
-      assert_includes schema_definition.attributes, SchemaAttribute.new(:email)
+      assert_includes schema_definition, SchemaAttribute.new(:email)
     end
 
     def test_preventing_duplicate_attributes
@@ -23,16 +23,16 @@ module Transformer
       schema_definition.attribute(:email)
       schema_definition.attribute(:email)
 
-      assert_equal 1, schema_definition.attributes.length
+      assert_equal 1, schema_definition.size
     end
 
     def test_adding_multiple_attributes_at_once
       schema_definition = SchemaDefinition.new
       schema_definition.attributes(:first_name, :last_name)
 
-      assert_equal 2, schema_definition.attributes.length
-      assert_includes schema_definition.attributes, SchemaAttribute.new(:first_name)
-      assert_includes schema_definition.attributes, SchemaAttribute.new(:last_name)
+      assert_equal 2, schema_definition.size
+      assert_includes schema_definition, SchemaAttribute.new(:first_name)
+      assert_includes schema_definition, SchemaAttribute.new(:last_name)
     end
   end
 end
